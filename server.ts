dcc2f4445adb5e5c11bc955b8e393dc88cf5ccb4
Jongwon Choi <h2511129@joongdong.hs.kr>
2026-08-24 12:19:03 +0900

    initial commit

---
+++ server.ts
@@ -0,0 +1,16 @@
+//@ts-expect-error no typedefs
+import { server as wisp } from '@mercuryworkshop/wisp-js/server'
+import http from 'node:http'
+
+const wispserver = http.createServer((req, res) => {
+	res.writeHead(200, { "Content-Type": "text/plain" });
+	res.end("wisp server js rewrite");
+});
+wisp.options.allow_private_ips = true;
+wisp.options.allow_loopback_ips = true;
+
+wispserver.on("upgrade", (req, socket, head) => {
+	wisp.routeRequest(req, socket, head);
+});
+
+wispserver.listen(4141);
